b09c3ace3779d230113a1838a76dec402a43ec07
Julio Sampaio <juliodssampaio@gmail.com>
2020-07-02 00:26:00 -0300

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'humanize-duration.js',
+    library: '',
+    libraryTarget: 'umd',
+    globalObject: 'this',
   },
 };
